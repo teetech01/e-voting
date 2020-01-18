95c8a758d95730201802bfa0ec169780bd0ef574
Sanusi Yusuf Korede <sanusikorede@gmail.com>
2020-01-18 22:28:16 +0100

    first commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,35 @@
+<footer class="footer">
+  <div class=" container-fluid ">
+    <nav>
+      <ul>
+        <li>
+          <a href="https://www.creative-tim.com" target="_blank">
+            {{__(" Creative Tim")}}
+          </a>
+        </li>
+        <li>
+          <a href="http://presentation.creative-tim.com" target="_blank">
+            {{__(" About Us")}}
+          </a>
+        </li>
+        <li>
+          <a href="http://blog.creative-tim.com" target="_blank">
+            {{__(" Blog")}}
+          </a>
+        </li>
+        <li>
+          <a href="https://www.updivision.com" target="_blank">
+            {{__(" Updivision")}}</a>
+        </li>
+      </ul>
+    </nav>
+    <div class="copyright" id="copyright">
+      &copy;
+      <script>
+        document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()))
+      </script>, {{__(" Designed by")}}
+      <a href="https://skyusuf15.github.io" target="_blank">{{__(" SKY-Tech")}}</a>{{__(" . Coded by")}}
+      <a href="https://skyusuf15.github.io" target="_blank">{{__(" Sanusi Yusuf")}}</a>
+    </div>
+  </div>
+</footer>
